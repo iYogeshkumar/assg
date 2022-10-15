06c3221274c5180d93954afd355d33b1e51648d7
yogesh Kumar <iamyogeshkumar21@gmail.com>
2022-10-15 15:41:37 +0530

    backend

---
+++ assgbackend/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            <Link className='navhead' to="/"><h1>Store</h1></Link>
+            <Link className='navcart' to="/cart">Cart</Link>
+        </nav>
+    );
+};
+
+export default Navbar;
